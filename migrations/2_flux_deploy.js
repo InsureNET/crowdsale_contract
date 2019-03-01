950ee48c64fa3c8f6212ac6315ea2d1fd6088790
CodyCorn <codycorn@protonmail.com>
2019-03-02 03:40:18 +0530

    Level one migration successful

---
+++ migrations/2_flux_deploy.js
@@ -0,0 +1,10 @@
+const flux_token = artifacts.require("FluxToken");
+  /* Token Parameter */ 
+  const name = "Flux Token";
+  const symbol = "FT";
+  const decimals = 18;
+
+module.exports = function(deployer) {
+  /* Passing constructor value */
+  deployer.deploy(flux_token, name, symbol, decimals);
+};
