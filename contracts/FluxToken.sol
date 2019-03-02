642e3a748f1446a0f473d1423443f91c5802af6a
CodyCorn <codycorn@protonmail.com>
2019-03-02 08:40:58 +0530

    Test file and covert the token in to Mintable and Pausable token

--- contracts/FluxToken.sol
+++ contracts/FluxToken.sol
@@ -2,10 +2,10 @@ pragma solidity ^0.5.0;
 
 /* Defining token attributes */
 import "/home/beach/node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
-/* Defining all ERC20 functions */
-import "/home/beach/node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "/home/beach/node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+import "/home/beach/node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Pausable.sol";
 
-contract FluxToken is ERC20Detailed, ERC20 {
+contract FluxToken is ERC20Mintable, ERC20Pausable, ERC20Detailed {
   constructor(string memory name, string memory symbol, uint8 decimals) 
      ERC20Detailed(name, symbol, decimals)
      public {}
---
+++ test/flux_token_test.js
@@ -0,0 +1,48 @@
+/* Utilising file */
+const flux_token = artifacts.require('FluxToken');
+
+/* A BDD/TDD assertion library */
+require('chai')
+  .should()
+
+contract('flux_token', accounts => {
+
+  /* Deploying new version of contract */
+  beforeEach(async function() {
+    // Passing arguments while deploying contract 
+    this.token = await flux_token.new('Flux Token', 'FT', 18);
+  });
+
+  describe('token_details', function() {     
+    /* Testing name of token */
+    it('testing_for_correct_name', async function() { 
+      // Getting token name
+      const token_name = await this.token.name();
+
+      // Checking if name is correct
+      // assert.equal(token_name, 'Tree Token');
+      // Alternate for above code line
+      token_name.should.equal('Flux Token');
+    });
+
+
+    /* Testing symbol of token */
+    it('testing_for_correct_symbol', async function() { 
+      const token_symbol = await this.token.symbol();
+
+      assert.equal(token_symbol, 'FT');
+    });
+    
+
+    /* Testing decimals of token */
+    it('testing_for_decimals', async function() { 
+      const token_decimals = await this.token.decimals();
+
+      assert.equal(token_decimals, 18);
+    });
+
+  
+  });
+  
+
+});
