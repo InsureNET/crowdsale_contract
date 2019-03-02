330822b33ced162ea4dbeb7ebbb27c9c029e9c8b
CodyCorn <codycorn@protonmail.com>
2019-03-02 09:58:34 +0530

    Constructor for crowdsale file added

--- contracts/FluxTokenCrowdsale.sol
+++ contracts/FluxTokenCrowdsale.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.5.0;
 
-import "/home/beach/node_modules/openzeppelin-solidity/contracts/crowdsale/Crowdsale.sol"
+import "/home/beach/node_modules/openzeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
 
-contract FluxTokenCrowdsale {
-  constructor() public {
-  }
+contract FluxTokenCrowdsale is Crowdsale {
+  constructor(uint256 rate, address payable wallet, IERC20 token) 
+    Crowdsale(rate, wallet, token)
+    public { }
 }
