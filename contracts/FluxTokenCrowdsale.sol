e97b208ea14c3842cf93d5d05efa4be61cd855e6
CodyCorn <codycorn@protonmail.com>
2019-03-02 13:13:58 +0530

    Crowdsale converted to minted crowdsale by adding import

--- contracts/FluxTokenCrowdsale.sol
+++ contracts/FluxTokenCrowdsale.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.5.0;
 
 import "/home/beach/node_modules/openzeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
+import "/home/beach/node_modules/openzeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol";
 
-contract FluxTokenCrowdsale is Crowdsale {
+contract FluxTokenCrowdsale is Crowdsale, MintedCrowdsale {
   constructor(uint256 rate, address payable wallet, IERC20 token) 
     Crowdsale(rate, wallet, token)
     public { }
