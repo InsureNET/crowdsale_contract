8a0f20ffd8bc545c0bcaa08be08f945b3668b227
CodyCorn <codycorn@protonmail.com>
2019-03-02 09:08:40 +0530

    Crowdsale file added

---
+++ contracts/FluxTokenCrowdsale.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.0;
+
+import "/home/beach/node_modules/openzeppelin-solidity/contracts/crowdsale/Crowdsale.sol"
+
+contract FluxTokenCrowdsale {
+  constructor() public {
+  }
+}
