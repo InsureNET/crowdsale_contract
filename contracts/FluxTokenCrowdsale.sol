5e20d78adb913ba43cab02c9e7855c909adbcb8d
CodyCorn <codycorn@protonmail.com>
2019-03-04 19:53:54 +0530

    CappedCrowd sale added to Crowdsale contract

--- contracts/FluxTokenCrowdsale.sol
+++ contracts/FluxTokenCrowdsale.sol
@@ -2,8 +2,9 @@ pragma solidity ^0.5.0;
 
 import "/home/beach/node_modules/openzeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
 import "/home/beach/node_modules/openzeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol";
+import "/home/beach/node_modules/openzeppelin-solidity/contracts/crowdsale/validation/CappedCrowdsale.sol";
 
-contract FluxTokenCrowdsale is Crowdsale, MintedCrowdsale {
+contract FluxTokenCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale {
   constructor(uint256 rate, address payable wallet, IERC20 token) 
     Crowdsale(rate, wallet, token)
     public { }
