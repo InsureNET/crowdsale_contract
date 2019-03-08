b8471941bf6e8a850be9884b97a42e96b7234377
CodyCorn <codycorn@protonmail.com>
2019-03-08 13:22:48 +0530

    Pasuable crowdsale imported

--- contracts/SlotheetokenCrowdsale.sol
+++ contracts/SlotheetokenCrowdsale.sol
@@ -4,11 +4,12 @@ import "../openzeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
 import "../openzeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol";
 import "../openzeppelin-solidity/contracts/crowdsale/validation/CappedCrowdsale.sol";
 import "../openzeppelin-solidity/contracts/crowdsale/distribution/RefundableCrowdsale.sol";
+import "../openzeppelin-solidity/contracts/crowdsale/validation/PausableCrowdsale.sol";
 import "../openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 // Do not alter the order of the imported extensions
 // Got no idea why Linerization error pops out for changing order
-contract SlotheeTokenCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale, RefundableCrowdsale, Ownable {
+contract SlotheeTokenCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale, RefundableCrowdsale, PausableCrowdsale,  Ownable {
   // Crowdsale contructor
   // Capped crowdsale constructor
   constructor(uint256 rate, address payable wallet, IERC20 token, uint256 cap, uint256 goal) 
